701408e8cd1a71cb306fb5ff22abd5bebb0dc82b
william <wzm0002@alu.medac.es>
2024-03-11 13:06:47 +0100

    último cambio antes del almuerzo

--- src/pruebasunitarias/PruebasUnitarias.java
+++ src/pruebasunitarias/PruebasUnitarias.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pruebasunitarias;
 
 /**
  *
- * @author William Zapata
+ * @author WilliamZapataMolina
  */
 public class PruebasUnitarias {
 

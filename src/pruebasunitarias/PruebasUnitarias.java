c490826fe18032dbac42270e4346861760cfc431
william <wzm0002@alu.medac.es>
2024-05-04 10:21:22 +0200

    Se agregó la resta en el main

--- src/pruebasunitarias/PruebasUnitarias.java
+++ src/pruebasunitarias/PruebasUnitarias.java
@@ -1,4 +1,3 @@
-
 package pruebasunitarias;
 
 /**
@@ -10,8 +9,12 @@ public class PruebasUnitarias {
     public static void main(String[] args) {
 
         Calculadora calculator = new Calculadora();
-        int resultado = (int) calculator.sumar(3,5);
-        System.out.println("Resultado:"+ resultado);
+        int resultado = (int) calculator.sumar(3, 5);
+        System.out.println("El resultado de la suma es:" + resultado);
+
+        Calculadora calculator1 = new Calculadora();
+        int resultado1 = (int) calculator1.restar(8, 3);
+        System.out.println("El resultado de la resta es:" + resultado1);
 
     }
 
